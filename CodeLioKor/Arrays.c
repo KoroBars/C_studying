88b9f8d678f4ac589f0bb9c739aefb4c8af960a2
KoroBars <korodog@yandex.ru>
2023-02-26 21:56:53 +0300

    New 2 Tasks on arrays added

--- CodeLioKor/Arrays.c
+++ CodeLioKor/Arrays.c
@@ -33,6 +33,31 @@ int count_how_many_times_number_occurred(const int arr[], int length, int number
     return sum;
 }
 
+// №4 Написать функцию, которая получает на вход массив, его длину и число, а затем считает, сколько чисел в этом
+// массиве больше, чем переданное число.
+int count_how_many_numbers_are_greater_than_number(const int arr[], int length, int number) {
+    int amount = 0;
+    for (int i = 0; i < length; i++) {
+        if (arr[i] > number) {
+            amount = amount + 1;
+        }
+    }
+    return amount;
+}
+
+// №5 Написать функцию, которая получает на вход массив, оканчивающийся нулем. Посчитать кол - во четных чисел в нём.
+int count_even_numbers(const int arr[]) {
+    int amount = 0;
+    int i = 0;
+    while (arr[i] != 0) {
+        if (arr[i] % 2 == 0) {
+            amount += 1;
+        }
+        i += 1;
+    }
+    return amount;
+}
+
 int main() {
     int arr[] = {1, 26, 0, 4, -5, 1};
     bool result_1 = array_has_number(arr, 6, 26);
@@ -43,4 +68,11 @@ int main() {
 
     int result_3 = count_how_many_times_number_occurred(arr, 6, -5);
     printf("Answer #3: %d\n", result_3);
+
+    int result_4 = count_how_many_numbers_are_greater_than_number(arr, 6, -5);
+    printf("Answer #4: %d\n", result_4);
+
+    int arr_ends_with_zero[] = {26, -2, 9, 26, 31, 8, 555, 15, 23, 228, 0};
+    int result_5 = count_even_numbers(arr_ends_with_zero);
+    printf("Answer #5: %d\n", result_5);
 }
