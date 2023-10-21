115cda552b94081ae93d84b9effb4b34db25fa64
KoroBars <korodog@yandex.ru>
2023-10-21 20:44:45 +0300

    Minimum search functions were added

--- CodeLioKor/Array_Print_Copy_and_Reverse.c
+++ CodeLioKor/Array_Print_Copy_and_Reverse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void print_arr (int size, int arr[]) {
     printf("{");
@@ -22,10 +23,49 @@ void print_arr_reverse(int size, int arr[]) {
     printf("}");
 }
 
+int min_el_of_arr(int size, int arr[]) {
+    for (int i = 0; i < size; i++) {
+        bool el_with_index_i_is_minimum = true;
+        for (int j = 0; j < size; j++) {
+            if (arr[j] < arr[i]) {
+                el_with_index_i_is_minimum = false;
+            }
+        }
+        if (el_with_index_i_is_minimum == true) {
+            return arr[i];
+        }
+    }
+}
+
+// 1. Записать 1 элемент массива как минимальный. 2. Далее сравниваем каждый элемент массива с минимальным. 3. Если
+// в массиве присутствует такой элемент, который меньше минимального, то такой элемент считается новым минимальным значением.
+
+int min_el_of_arr_optimal(int size, int arr[]) {
+    int min = arr[0];
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
 int main() {
 
     const int size = 8;
     int arr_of_birthdays[] = {26, 31, 18, 19, 23, 15, 23, 31};
+
+    int min = min_el_of_arr(size, arr_of_birthdays);
+    int min_optimal = min_el_of_arr_optimal(size, arr_of_birthdays);
+    printf("%d %d\n", min, min_optimal);
+
+    int arr_size = 5;
+    int arr[] = {5, 5, 5, 5, 5};
+    int min_2 = min_el_of_arr(arr_size, arr);
+    int min_2_optimal = min_el_of_arr_optimal(arr_size, arr);
+    printf("%d %d\n", min_2, min_2_optimal);
+
+    printf("\n");
 
     int arr_of_birthdays_2[size * 2];
     for (int i = 0; i < size * 2; i++) {
@@ -54,4 +94,6 @@ int main() {
     printf("\n");
     print_arr_reverse(size * 3, arr_of_birthdays_3);
     printf("\n");
+
+    return 0;
 }
